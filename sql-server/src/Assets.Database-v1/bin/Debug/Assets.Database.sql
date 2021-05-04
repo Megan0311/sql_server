ea54a4a12f2817858a4f0f696bc420df0d6ceab0
Megan0311 <47632461+Megan0311@users.noreply.github.com>
2021-05-04 14:25:52 +0800

    Add files via upload

---
+++ sql-server/src/Assets.Database-v1/bin/Debug/Assets.Database.sql
@@ -0,0 +1,76 @@
+﻿/*
+Assets.Database 的部署指令碼
+
+這段程式碼由工具產生。
+變更這個檔案可能導致不正確的行為，而且如果重新產生程式碼，
+變更將會遺失。
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "Assets.Database"
+:setvar DefaultFilePrefix "Assets.Database"
+:setvar DefaultDataPath "C:\Users\happy.DESKTOP-7VOR5SQ\AppData\Local\Microsoft\VisualStudio\SSDT\Assets.Database"
+:setvar DefaultLogPath "C:\Users\happy.DESKTOP-7VOR5SQ\AppData\Local\Microsoft\VisualStudio\SSDT\Assets.Database"
+
+GO
+:on error exit
+GO
+/*
+偵測 SQLCMD 模式，如果不支援 SQLCMD 模式，則停用指令碼執行。
+若要在啟用 SQLCMD 模式後重新啟用指令碼，請執行以下:
+SET NOEXEC OFF; 
+*/
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'必須啟用 SQLCMD 模式才能成功執行此指令碼。';
+        SET NOEXEC ON;
+    END
+
+
+GO
+USE [$(DatabaseName)];
+
+
+GO
+
+USE [$(DatabaseName)];
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM AssetTypes)
+BEGIN
+
+    INSERT INTO [dbo].[AssetTypes] (AssetTypeDescription)
+    VALUES ('Laptop')
+
+    INSERT INTO [dbo].[AssetTypes] (AssetTypeDescription)
+    VALUES ('Monitor')
+
+    INSERT INTO [dbo].[AssetTypes] (AssetTypeDescription)
+    VALUES ('Phone')
+
+END
+GO
+IF NOT EXISTS (SELECT TOP 1 1 FROM Locations)
+BEGIN
+
+    INSERT INTO [dbo].[Locations] (Country, PostalCode, AddressLine1)
+    VALUES ('USA', 'DC 20500', '1600 Pennsylvania Ave NW')
+
+    INSERT INTO [dbo].[Locations] (Country, PostalCode, AddressLine1)
+    VALUES ('UK', 'SW1A 0AA', 'Houses of Parliament')
+
+END
+GO
+
+GO
+PRINT N'更新完成。';
+
+
+GO
---
+++ sql-server/src/Assets.Database-v1/bin/Debug/Assets.Database_Create.sql
@@ -0,0 +1,354 @@
+﻿/*
+Assets.Database 的部署指令碼
+
+這段程式碼由工具產生。
+變更這個檔案可能導致不正確的行為，而且如果重新產生程式碼，
+變更將會遺失。
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "Assets.Database"
+:setvar DefaultFilePrefix "Assets.Database"
+:setvar DefaultDataPath ""
+:setvar DefaultLogPath ""
+
+GO
+:on error exit
+GO
+/*
+偵測 SQLCMD 模式，如果不支援 SQLCMD 模式，則停用指令碼執行。
+若要在啟用 SQLCMD 模式後重新啟用指令碼，請執行以下:
+SET NOEXEC OFF; 
+*/
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'必須啟用 SQLCMD 模式才能成功執行此指令碼。';
+        SET NOEXEC ON;
+    END
+
+
+GO
+USE [master];
+
+
+GO
+
+IF (DB_ID(N'$(DatabaseName)') IS NOT NULL) 
+BEGIN
+    ALTER DATABASE [$(DatabaseName)]
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE [$(DatabaseName)];
+END
+
+GO
+PRINT N'正在建立 $(DatabaseName)...'
+GO
+CREATE DATABASE [$(DatabaseName)] COLLATE SQL_Latin1_General_CP1_CI_AS
+GO
+USE [$(DatabaseName)];
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET ANSI_NULLS ON,
+                ANSI_PADDING ON,
+                ANSI_WARNINGS ON,
+                ARITHABORT ON,
+                CONCAT_NULL_YIELDS_NULL ON,
+                NUMERIC_ROUNDABORT OFF,
+                QUOTED_IDENTIFIER ON,
+                ANSI_NULL_DEFAULT ON,
+                CURSOR_DEFAULT LOCAL,
+                RECOVERY FULL,
+                CURSOR_CLOSE_ON_COMMIT OFF,
+                AUTO_CREATE_STATISTICS ON,
+                AUTO_SHRINK OFF,
+                AUTO_UPDATE_STATISTICS ON,
+                RECURSIVE_TRIGGERS OFF 
+            WITH ROLLBACK IMMEDIATE;
+        ALTER DATABASE [$(DatabaseName)]
+            SET AUTO_CLOSE OFF 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET ALLOW_SNAPSHOT_ISOLATION OFF;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET READ_COMMITTED_SNAPSHOT OFF 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET AUTO_UPDATE_STATISTICS_ASYNC OFF,
+                PAGE_VERIFY NONE,
+                DATE_CORRELATION_OPTIMIZATION OFF,
+                DISABLE_BROKER,
+                PARAMETERIZATION SIMPLE,
+                SUPPLEMENTAL_LOGGING OFF 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF IS_SRVROLEMEMBER(N'sysadmin') = 1
+    BEGIN
+        IF EXISTS (SELECT 1
+                   FROM   [master].[dbo].[sysdatabases]
+                   WHERE  [name] = N'$(DatabaseName)')
+            BEGIN
+                EXECUTE sp_executesql N'ALTER DATABASE [$(DatabaseName)]
+    SET TRUSTWORTHY OFF,
+        DB_CHAINING OFF 
+    WITH ROLLBACK IMMEDIATE';
+            END
+    END
+ELSE
+    BEGIN
+        PRINT N'無法修改資料庫設定。您必須是 SysAdmin 才能套用這些設定。';
+    END
+
+
+GO
+IF IS_SRVROLEMEMBER(N'sysadmin') = 1
+    BEGIN
+        IF EXISTS (SELECT 1
+                   FROM   [master].[dbo].[sysdatabases]
+                   WHERE  [name] = N'$(DatabaseName)')
+            BEGIN
+                EXECUTE sp_executesql N'ALTER DATABASE [$(DatabaseName)]
+    SET HONOR_BROKER_PRIORITY OFF 
+    WITH ROLLBACK IMMEDIATE';
+            END
+    END
+ELSE
+    BEGIN
+        PRINT N'無法修改資料庫設定。您必須是 SysAdmin 才能套用這些設定。';
+    END
+
+
+GO
+ALTER DATABASE [$(DatabaseName)]
+    SET TARGET_RECOVERY_TIME = 0 SECONDS 
+    WITH ROLLBACK IMMEDIATE;
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET FILESTREAM(NON_TRANSACTED_ACCESS = OFF),
+                CONTAINMENT = NONE 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET AUTO_CREATE_STATISTICS ON(INCREMENTAL = OFF),
+                MEMORY_OPTIMIZED_ELEVATE_TO_SNAPSHOT = OFF,
+                DELAYED_DURABILITY = DISABLED 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET QUERY_STORE (QUERY_CAPTURE_MODE = ALL, DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_PLANS_PER_QUERY = 200, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 367), MAX_STORAGE_SIZE_MB = 100) 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE [$(DatabaseName)]
+            SET QUERY_STORE = OFF 
+            WITH ROLLBACK IMMEDIATE;
+    END
+
+
+GO
+IF EXISTS (SELECT 1
+           FROM   [master].[dbo].[sysdatabases]
+           WHERE  [name] = N'$(DatabaseName)')
+    BEGIN
+        ALTER DATABASE SCOPED CONFIGURATION SET MAXDOP = 0;
+        ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET MAXDOP = PRIMARY;
+        ALTER DATABASE SCOPED CONFIGURATION SET LEGACY_CARDINALITY_ESTIMATION = OFF;
+        ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET LEGACY_CARDINALITY_ESTIMATION = PRIMARY;
+        ALTER DATABASE SCOPED CONFIGURATION SET PARAMETER_SNIFFING = ON;
+        ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET PARAMETER_SNIFFING = PRIMARY;
+        ALTER DATABASE SCOPED CONFIGURATION SET QUERY_OPTIMIZER_HOTFIXES = OFF;
+        ALTER DATABASE SCOPED CONFIGURATION FOR SECONDARY SET QUERY_OPTIMIZER_HOTFIXES = PRIMARY;
+    END
+
+
+GO
+IF fulltextserviceproperty(N'IsFulltextInstalled') = 1
+    EXECUTE sp_fulltext_database 'enable';
+
+
+GO
+PRINT N'正在建立 [dbo].[Locations]...';
+
+
+GO
+CREATE TABLE [dbo].[Locations] (
+    [LocationId]   INT            IDENTITY (1, 1) NOT NULL,
+    [Country]      NVARCHAR (50)  NOT NULL,
+    [PostalCode]   NVARCHAR (50)  NULL,
+    [AddressLine1] NVARCHAR (500) NULL,
+    PRIMARY KEY CLUSTERED ([LocationId] ASC)
+);
+
+
+GO
+PRINT N'正在建立 [dbo].[AssetTypes]...';
+
+
+GO
+CREATE TABLE [dbo].[AssetTypes] (
+    [AssetTypeId]          INT           IDENTITY (1, 1) NOT NULL,
+    [AssetTypeDescription] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([AssetTypeId] ASC)
+);
+
+
+GO
+PRINT N'正在建立 [dbo].[Assets]...';
+
+
+GO
+CREATE TABLE [dbo].[Assets] (
+    [AssetId]          INT            IDENTITY (1, 1) NOT NULL,
+    [AssetTypeId]      INT            NOT NULL,
+    [LocationId]       INT            NOT NULL,
+    [PurchaseDate]     DATE           NULL,
+    [PurchasePrice]    DECIMAL (9, 2) NULL,
+    [AssetTag]         NVARCHAR (50)  NULL,
+    [AssetDescription] NVARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([AssetId] ASC)
+);
+
+
+GO
+PRINT N'正在建立 [dbo].[FK_Assets_To_AssetTypes]...';
+
+
+GO
+ALTER TABLE [dbo].[Assets]
+    ADD CONSTRAINT [FK_Assets_To_AssetTypes] FOREIGN KEY ([AssetTypeId]) REFERENCES [dbo].[AssetTypes] ([AssetTypeId]);
+
+
+GO
+PRINT N'正在建立 [dbo].[FK_Assets_To_Locations]...';
+
+
+GO
+ALTER TABLE [dbo].[Assets]
+    ADD CONSTRAINT [FK_Assets_To_Locations] FOREIGN KEY ([LocationId]) REFERENCES [dbo].[Locations] ([LocationId]);
+
+
+GO
+
+USE [$(DatabaseName)];
+
+IF NOT EXISTS (SELECT TOP 1 1 FROM AssetTypes)
+BEGIN
+
+    INSERT INTO [dbo].[AssetTypes] (AssetTypeDescription)
+    VALUES ('Laptop')
+
+    INSERT INTO [dbo].[AssetTypes] (AssetTypeDescription)
+    VALUES ('Monitor')
+
+    INSERT INTO [dbo].[AssetTypes] (AssetTypeDescription)
+    VALUES ('Phone')
+
+END
+GO
+IF NOT EXISTS (SELECT TOP 1 1 FROM Locations)
+BEGIN
+
+    INSERT INTO [dbo].[Locations] (Country, PostalCode, AddressLine1)
+    VALUES ('USA', 'DC 20500', '1600 Pennsylvania Ave NW')
+
+    INSERT INTO [dbo].[Locations] (Country, PostalCode, AddressLine1)
+    VALUES ('UK', 'SW1A 0AA', 'Houses of Parliament')
+
+END
+GO
+
+GO
+DECLARE @VarDecimalSupported AS BIT;
+
+SELECT @VarDecimalSupported = 0;
+
+IF ((ServerProperty(N'EngineEdition') = 3)
+    AND (((@@microsoftversion / power(2, 24) = 9)
+          AND (@@microsoftversion & 0xffff >= 3024))
+         OR ((@@microsoftversion / power(2, 24) = 10)
+             AND (@@microsoftversion & 0xffff >= 1600))))
+    SELECT @VarDecimalSupported = 1;
+
+IF (@VarDecimalSupported > 0)
+    BEGIN
+        EXECUTE sp_db_vardecimal_storage_format N'$(DatabaseName)', 'ON';
+    END
+
+
+GO
+ALTER DATABASE [$(DatabaseName)]
+    SET MULTI_USER 
+    WITH ROLLBACK IMMEDIATE;
+
+
+GO
+PRINT N'更新完成。';
+
+
+GO
